a92b7cc5ff656002265265d51cb6f82068963f2a
Thilina Dinelka <121526622+dinelkathilina@users.noreply.github.com>
2024-06-14 15:55:03 +0530

    password rout set

--- dnlk-app/src/main.jsx
+++ dnlk-app/src/main.jsx
@@ -8,17 +8,25 @@ import {
 } from "react-router-dom";
 import { RootLayout } from './Components/RootLayout.jsx';
 import { Email_login } from './Components/Email_login.jsx';
+import ErrorPage from './Components/ErrorPage.jsx';
+import { Password_login } from './Components/Password_login.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
+    errorElement:<ErrorPage />,
     children: [
       {
         index: true,
         element: <Email_login />,
       },
+      {
+        path:'/password',
+        element:<Password_login />,
+      },
     ],
+    
   },
 ]);
 
